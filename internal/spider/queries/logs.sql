876b38e60d2a5bafb7d2a58455fef537bb9d7d1c
Serge Paskal <3990195+srgpsk@users.noreply.github.com>
2025-06-29 22:10:20 +0000

    feat: add logs helper query

--- internal/spider/queries/logs.sql
+++ internal/spider/queries/logs.sql
@@ -1,5 +1,5 @@
 -- name: LogMessage :exec
-INSERT INTO spider_logs (log_type, target_id, url, message, details) 
+INSERT INTO spider_logs (log_type, target_id, url, message, details)
 VALUES (?, ?, ?, ?, ?);
 
 -- name: GetRecentLogs :many
@@ -10,5 +10,11 @@ LIMIT ?;
 -- name: GetLogsByTarget :many
 SELECT * FROM spider_logs 
 WHERE target_id = ? 
+ORDER BY created_at DESC 
+LIMIT ?;
+
+-- name: GetLogsByLevel :many
+SELECT * FROM spider_logs 
+WHERE log_type = ? 
 ORDER BY created_at DESC 
 LIMIT ?;
